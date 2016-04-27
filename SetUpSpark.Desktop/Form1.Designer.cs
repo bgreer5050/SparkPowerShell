e2779ac1da6ebf5542fc1581922ab406ddb94a94
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-04-27 06:49:08 -0500

    Set up Pi

--- SetUpSpark.Desktop/Form1.Designer.cs
+++ SetUpSpark.Desktop/Form1.Designer.cs
@@ -32,6 +32,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnConnect = new System.Windows.Forms.Button();
             this.lblMessage = new System.Windows.Forms.Label();
+            this.txtDNSServer = new System.Windows.Forms.TextBox();
+            this.btnSetDNSServer = new System.Windows.Forms.Button();
+            this.btnSetPassword = new System.Windows.Forms.Button();
+            this.btnSetTimeZone = new System.Windows.Forms.Button();
+            this.btnCopyTaskFile = new System.Windows.Forms.Button();
+            this.btnScheduleTasks = new System.Windows.Forms.Button();
+            this.btnSetDNSWiFi = new System.Windows.Forms.Button();
+            this.cmbPlant = new System.Windows.Forms.ComboBox();
+            this.txtNewName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNewName = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,6 +51,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // label1
             // 
@@ -52,7 +64,7 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(44, 94);
+            this.btnConnect.Location = new System.Drawing.Point(47, 75);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(75, 23);
             this.btnConnect.TabIndex = 2;
@@ -63,16 +75,135 @@
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(44, 150);
+            this.lblMessage.Location = new System.Drawing.Point(293, 373);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(0, 13);
             this.lblMessage.TabIndex = 3;
+            // 
+            // txtDNSServer
+            // 
+            this.txtDNSServer.Location = new System.Drawing.Point(46, 185);
+            this.txtDNSServer.Name = "txtDNSServer";
+            this.txtDNSServer.Size = new System.Drawing.Size(100, 20);
+            this.txtDNSServer.TabIndex = 4;
+            // 
+            // btnSetDNSServer
+            // 
+            this.btnSetDNSServer.Location = new System.Drawing.Point(55, 211);
+            this.btnSetDNSServer.Name = "btnSetDNSServer";
+            this.btnSetDNSServer.Size = new System.Drawing.Size(75, 23);
+            this.btnSetDNSServer.TabIndex = 5;
+            this.btnSetDNSServer.Text = "Set DNS";
+            this.btnSetDNSServer.UseVisualStyleBackColor = true;
+            this.btnSetDNSServer.Click += new System.EventHandler(this.btnSetDNSServer_Click);
+            // 
+            // btnSetPassword
+            // 
+            this.btnSetPassword.Location = new System.Drawing.Point(41, 124);
+            this.btnSetPassword.Name = "btnSetPassword";
+            this.btnSetPassword.Size = new System.Drawing.Size(102, 23);
+            this.btnSetPassword.TabIndex = 6;
+            this.btnSetPassword.Text = "Set Password";
+            this.btnSetPassword.UseVisualStyleBackColor = true;
+            this.btnSetPassword.Click += new System.EventHandler(this.btnSetPassword_Click);
+            // 
+            // btnSetTimeZone
+            // 
+            this.btnSetTimeZone.Location = new System.Drawing.Point(55, 261);
+            this.btnSetTimeZone.Name = "btnSetTimeZone";
+            this.btnSetTimeZone.Size = new System.Drawing.Size(141, 23);
+            this.btnSetTimeZone.TabIndex = 7;
+            this.btnSetTimeZone.Text = "Set Time Zone";
+            this.btnSetTimeZone.UseVisualStyleBackColor = true;
+            this.btnSetTimeZone.Click += new System.EventHandler(this.btnSetTimeZone_Click);
+            // 
+            // btnCopyTaskFile
+            // 
+            this.btnCopyTaskFile.Location = new System.Drawing.Point(55, 312);
+            this.btnCopyTaskFile.Name = "btnCopyTaskFile";
+            this.btnCopyTaskFile.Size = new System.Drawing.Size(173, 23);
+            this.btnCopyTaskFile.TabIndex = 8;
+            this.btnCopyTaskFile.Text = "Copy Task File";
+            this.btnCopyTaskFile.UseVisualStyleBackColor = true;
+            this.btnCopyTaskFile.Click += new System.EventHandler(this.btnCopyTaskFile_Click);
+            // 
+            // btnScheduleTasks
+            // 
+            this.btnScheduleTasks.Location = new System.Drawing.Point(296, 49);
+            this.btnScheduleTasks.Name = "btnScheduleTasks";
+            this.btnScheduleTasks.Size = new System.Drawing.Size(177, 23);
+            this.btnScheduleTasks.TabIndex = 9;
+            this.btnScheduleTasks.Text = "Schedule Tasks";
+            this.btnScheduleTasks.UseVisualStyleBackColor = true;
+            this.btnScheduleTasks.Click += new System.EventHandler(this.btnScheduleTasks_Click);
+            // 
+            // btnSetDNSWiFi
+            // 
+            this.btnSetDNSWiFi.Location = new System.Drawing.Point(159, 210);
+            this.btnSetDNSWiFi.Name = "btnSetDNSWiFi";
+            this.btnSetDNSWiFi.Size = new System.Drawing.Size(185, 23);
+            this.btnSetDNSWiFi.TabIndex = 10;
+            this.btnSetDNSWiFi.Text = "Set DNS WiFi";
+            this.btnSetDNSWiFi.UseVisualStyleBackColor = true;
+            this.btnSetDNSWiFi.Click += new System.EventHandler(this.btnSetDNSWiFi_Click);
+            // 
+            // cmbPlant
+            // 
+            this.cmbPlant.FormattingEnabled = true;
+            this.cmbPlant.Items.AddRange(new object[] {
+            "Minneapolis",
+            "Bedford",
+            "Middletown",
+            "Humboldt"});
+            this.cmbPlant.Location = new System.Drawing.Point(375, 277);
+            this.cmbPlant.Name = "cmbPlant";
+            this.cmbPlant.Size = new System.Drawing.Size(121, 21);
+            this.cmbPlant.TabIndex = 11;
+            this.cmbPlant.ValueMemberChanged += new System.EventHandler(this.cmbPlant_ValueMemberChanged);
+            this.cmbPlant.SelectedValueChanged += new System.EventHandler(this.cmbPlant_SelectedValueChanged);
+            // 
+            // txtNewName
+            // 
+            this.txtNewName.Location = new System.Drawing.Point(373, 168);
+            this.txtNewName.Name = "txtNewName";
+            this.txtNewName.Size = new System.Drawing.Size(100, 20);
+            this.txtNewName.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Enter New Name";
+            // 
+            // btnNewName
+            // 
+            this.btnNewName.Location = new System.Drawing.Point(378, 195);
+            this.btnNewName.Name = "btnNewName";
+            this.btnNewName.Size = new System.Drawing.Size(75, 23);
+            this.btnNewName.TabIndex = 14;
+            this.btnNewName.Text = "Set Name";
+            this.btnNewName.UseVisualStyleBackColor = true;
+            this.btnNewName.Click += new System.EventHandler(this.btnNewName_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 395);
+            this.Controls.Add(this.btnNewName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNewName);
+            this.Controls.Add(this.cmbPlant);
+            this.Controls.Add(this.btnSetDNSWiFi);
+            this.Controls.Add(this.btnScheduleTasks);
+            this.Controls.Add(this.btnCopyTaskFile);
+            this.Controls.Add(this.btnSetTimeZone);
+            this.Controls.Add(this.btnSetPassword);
+            this.Controls.Add(this.btnSetDNSServer);
+            this.Controls.Add(this.txtDNSServer);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.label1);
@@ -90,6 +221,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtDNSServer;
+        private System.Windows.Forms.Button btnSetDNSServer;
+        private System.Windows.Forms.Button btnSetPassword;
+        private System.Windows.Forms.Button btnSetTimeZone;
+        private System.Windows.Forms.Button btnCopyTaskFile;
+        private System.Windows.Forms.Button btnScheduleTasks;
+        private System.Windows.Forms.Button btnSetDNSWiFi;
+        private System.Windows.Forms.ComboBox cmbPlant;
+        private System.Windows.Forms.TextBox txtNewName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNewName;
     }
 }
 
--- SetUpSpark.Desktop/Form1.cs
+++ SetUpSpark.Desktop/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Management.Automation;
@@ -15,6 +16,7 @@ namespace SetUpSpark.Desktop
 {
     public partial class Form1 : Form
     {
+        SparkPi pi;
 
         public string Message
         {
@@ -31,8 +33,126 @@ namespace SetUpSpark.Desktop
         private void btnConnect_Click(object sender, EventArgs e)
         {
             Form1 frm = (Form1)this.FindForm();
-            SparkPi pi = new SparkPi("10.0.0.82", frm);
-            pi.Connect("10.0.0.82");
+            pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            pi.Connect();
+        }
+
+        private void btnSetPassword_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSetTimeZone_Click(object sender, EventArgs e)
+        {
+            if (pi == null)
+            {
+                Form1 frm = (Form1)this.FindForm();
+                pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            }
+            pi.SetTimeZone();
+        }
+
+        private void btnCopyTaskFile_Click(object sender, EventArgs e)
+        {
+
+            System.IO.File.Copy(@"c:\Temp\SyncTime.ps1", @"\\192.168.1.109\c$\SyncTime2.ps1");
+            MessageBox.Show("NOT IMPLEMENTED");
+            //if (pi == null)
+            //{
+            //    Form1 frm = (Form1)this.FindForm();
+            //    pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            //}
+            //pi.CopyTimeTaskToPi();
+
+
+        }
+
+        private void btnScheduleTasks_Click(object sender, EventArgs e)
+        {
+            if (pi == null)
+            {
+                Form1 frm = (Form1)this.FindForm();
+                pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            }
+            pi.ScheduleTasks();
+            MessageBox.Show("Make Sure SyncTime.ps1 exists in the root of the device.");
+        }
+
+        private void btnSetDNSServer_Click(object sender, EventArgs e)
+        {
+            if (pi == null)
+            {
+                Form1 frm = (Form1)this.FindForm();
+                pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            }
+            pi.SetDNSEthernet();
+        }
+
+        private void btnSetDNSWiFi_Click(object sender, EventArgs e)
+        {
+            if (pi == null)
+            {
+                Form1 frm = (Form1)this.FindForm();
+                pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+            }
+            pi.SetDNSWiFi();
+        }
+
+        private void cmbPlant_ValueMemberChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine("kdhbfvkhdf");
+        }
+
+        private void cmbPlant_SelectedValueChanged(object sender, EventArgs e)
+        {
+            switch(this.cmbPlant.SelectedItem.ToString())
+            {
+                case "Minneapolis":
+                    MessageBox.Show("1");
+
+                    break;
+
+                case "Bedford":
+                    MessageBox.Show("2");
+
+                    break;
+
+                case "Middletown":
+                    MessageBox.Show("3");
+
+                    break;
+
+                case "Humboldt":
+                    MessageBox.Show("4");
+
+                    break;
+
+                default:
+                    MessageBox.Show("ERROR");
+
+                    break;
+            }
+
+            MessageBox.Show(this.cmbPlant.SelectedItem.ToString());
+        }
+
+        private void btnNewName_Click(object sender, EventArgs e)
+        {
+            Form1 frm = (Form1)this.FindForm();
+            if (txtNewName.Text != "")
+            {
+                if (pi == null)
+                {
+                     
+                    pi = new SparkPi("192.168.1.109", "192.168.1.109", frm);
+                }
+                pi.ChangeName(txtNewName.Text);
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            this.pi.IPAddress = textBox1.Text.Trim();
         }
     }
 
@@ -42,21 +162,108 @@ namespace SetUpSpark.Desktop
     {
         private string ipAddress;
 
+        private string _domainName;
+
+        public string DomainName
+        {
+            get {
+                return _domainName;
+            }
+            set { _domainName = value; }
+        }
+
+
+        private SecureString _securePassword;
+
+        public SecureString SecurePassword
+        {
+            get {
+                if(_securePassword == null)
+                {
+                    // Define the string value to assign to a new secure string.
+                    char[] chars = { 'p', '@', 's', 's', 'w', '0', 'r', 'd' };
+                    //char[] chars = { 's', 'p', 'a', 'r', 'k', 'p', 'i' };
+                    unsafe
+                    {
+                        fixed (char* pChars = chars)
+                        {
+                            _securePassword = new SecureString(pChars, chars.Length);
+                         }
+                    }
+                }
+                return _securePassword;
+            }
+            set { _securePassword = value; }
+        }
+
+        private WSManConnectionInfo _connectionInfo;
+
+        public WSManConnectionInfo connectionInfo
+        {
+            get {
+
+                if(_connectionInfo == null)
+                {
+
+                    connectionInfo = new WSManConnectionInfo(false, IPAddress, 5985, "/wsman", @"http://schemas.microsoft.com/powershell/Microsoft.PowerShell", RemoteMachineCredentials);
+
+                }
+
+                return _connectionInfo;
+            }
+            set { _connectionInfo = value; }
+        }
+
+        private PSCredential _remoteMachineCredentials;
+
+        public PSCredential RemoteMachineCredentials
+        {
+            get {
+                if(_remoteMachineCredentials == null)
+                {
+                    string strDomainAndUserName = ipAddress;
+                    strDomainAndUserName += @"\Administrator";
+                    _remoteMachineCredentials = new PSCredential(strDomainAndUserName, SecurePassword);
+                }
+
+                return _remoteMachineCredentials;
+            }
+            set { _remoteMachineCredentials = value; }
+        }
+
+
+
+
+
+
         public string IPAddress
         {
-            get { return ipAddress; }
-            set { ipAddress = value; }
+            get
+            {
+                return ipAddress;
+            }
+            set
+            {
+                ipAddress = value;
+            }
         }
 
         private Form1 form;
-        public SparkPi(string strIPAddress, Form1 frmMain)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="strIPAddress">The IP Address of the pi</param>
+        /// <param name="frmMain"></param>
+        public SparkPi(string strIPAddress,string strDomainName, Form1 frmMain)
         {
             form = frmMain;
+            this.IPAddress = strIPAddress;
+            this.DomainName = strDomainName;
         }
 
-        public bool Connect(string _ip)
+        public bool Connect()
         {
-            AddToTrustedHostsLists(_ip);
+            AddToTrustedHostsLists(this.IPAddress);
 
             // TODO Should not be hardset
             return false;
@@ -65,26 +272,8 @@ namespace SetUpSpark.Desktop
 
         public  void AddToTrustedHostsLists(string _ip)
         {
-            string domainAndUserName = _ip + @"\Administrator";
-
-            // Define the string value to assign to a new secure string.
-            //char[] chars = { 'p', '@', 's', 's', 'w', '0', 'r', 'd' };
-            char[] chars = { 's', 'p', 'a', 'r', 'k', 'p', 'i' };
-            SecureString securePassword;
-
-            unsafe
-            {
-                fixed (char* pChars = chars)
-                {
-                    securePassword = new SecureString(pChars, chars.Length);
-                }
-            }
-
-            PSCredential remoteMachineCredentials = new PSCredential(domainAndUserName, securePassword);
-
-            WSManConnectionInfo connectionInfo;
-            connectionInfo = new WSManConnectionInfo(false, _ip, 5985, "/wsman", @"http://schemas.microsoft.com/powershell/Microsoft.PowerShell", remoteMachineCredentials);
-
+           // string domainAndUserName = _ip + @"\Administrator";
+         
             Runspace rspace = RunspaceFactory.CreateRunspace();
             rspace.Open();
 
@@ -95,7 +284,254 @@ namespace SetUpSpark.Desktop
             form.Message = _result.ToString();
 
             pl.Dispose();
-             
+            
+
+
+            //Try running a command
+
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                runspace.Open();
+                //string command = CommandBuilder.Reboot();
+                // string command = CommandBuilder.SetTime();
+                Pipeline pipeline = runspace.CreatePipeline("get-date");
+                //Pipeline pipeline = runspace.CreatePipeline("set-date \"Thursday, February 11, 2016 1:03:00 PM\"");
+                // Pipeline pipeline = runspace.CreatePipeline("Set-Item WSMan:\\localhost\\Client\\TrustedHosts -Value 10.0.99.97");
+
+                var results = pipeline.Invoke();
+
+                pipeline = runspace.CreatePipeline("get-date");
+                var respGetDate = pipeline.Invoke();
+                form.Message = respGetDate[0].ToString();
+               // p.CurrentDateTime = respGetDate[0].ToString();
+                //txtResult.Text = results.Count.ToString();
+                //intSuccess += 1;
+               // p.TimeUpdatedSuccessfully = true;
+                //p.TimeUpdatedSuccessfullyAt = DateTime.Now.ToLongTimeString();
+                //p.TimeUpdatedLastAtTicks = DateTime.Now.Ticks;
+
+            }
+
+
         }
+
+        internal void SetTimeZone()
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                runspace.Open();
+                Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+              
+                var results = pipeline.Invoke();
+                if (results.Count > 0)
+                {
+
+                    form.Message = results[0].ToString();
+                }
+                else
+                {
+                    form.Message = "Timezone Updated!";
+                }
+            }
+        }
+
+        //internal void CopyTimeTaskToPi()
+        //{
+        //    System.Net.NetworkCredential nc = new System.Net.NetworkCredential("Administrator", "p@ssw0rd", "192.168.1.109");
+
+
+        //    using (new  NetworkConnection(@"\\server2\write", writeCredentials))
+        //    {
+        //        File.Copy(@"\\server\read\file", @"\\server2\write\file");
+        //    }
+
+
+        //    System.IO.File.Create(@"\\192.168.1.109\c$\SyncTime.ps1");
+
+        //    string strCommand = @"use K: \\192.168.1.109\c$ /USER:192.168.1.109\Administrator p@ssw0rd";
+
+        //    System.Text.StringBuilder sb = new StringBuilder();
+        //    sb.Append("use K: ");
+        //    sb.Append(@"\\192.168.1.109");
+        //    sb.Append(@"\c$");
+        //    sb.Append(" /USER:192.168.1.109");
+        //    sb.Append(@"\Administrator ");
+        //    //p @ssw0rd");
+
+        //    System.Diagnostics.Process p = new System.Diagnostics.Process();
+        //    System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(@"c:\Windows\System32\net.exe", sb.ToString());
+
+        //    p.StartInfo = info;
+
+        //    p.Exited += P_Exited;
+        //    p.Start();
+
+        //    p.ErrorDataReceived += P_ErrorDataReceived;
+        //    p.OutputDataReceived += P_OutputDataReceived;
+
+
+
+
+
+        //    //System.Diagnostics.Process.Start("net.exe", sb.ToString());
+        //    //System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
+
+        //    //using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+        //    //{
+
+        //    //    runspace.Open();
+        //    //    Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+
+        //    //    var results = pipeline.Invoke();
+        //    //    if (results.Count > 0)
+        //    //    {
+
+        //    //        form.Message = results[0].ToString();
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        form.Message = "Timezone Updated!";
+        //    //    }
+        //    //}
+        //}
+
+        private void P_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void P_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void P_Exited(object sender, EventArgs e)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        internal void ScheduleTasks()
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                runspace.Open();
+                //Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+                Pipeline  pipeline = runspace.CreatePipeline(@"schtasks /Create /SC ONSTART /TN TimeSync /TR c:\SyncTime.ps1");
+
+                var results = pipeline.Invoke();
+                if (results.Count > 0)
+                {
+
+                    form.Message = results[0].ToString();
+                }
+                else
+                {
+                    form.Message = "Timezone Updated!";
+                }
+            }
+        }
+
+        internal void SetDNSEthernet()
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                
+
+                runspace.Open();
+                //Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+                Pipeline pipeline = runspace.CreatePipeline(@"netsh interface ipv4 add dnsserver ""Ethernet"" address=10.0.0.1 index=1");
+
+                var results = pipeline.Invoke();
+                if (results.Count > 0)
+                {
+
+                    form.Message = results[0].ToString();
+                }
+                else
+                {
+                    form.Message = "Nothing Returned";
+                }
+            }
+        }
+
+        internal void SetDNSWiFi()
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                runspace.Open();
+                //Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+                Pipeline pipeline = runspace.CreatePipeline(@"netsh interface ipv4 add dnsserver ""Wi-Fi"" address=10.0.0.1 index=1");
+
+                var results = pipeline.Invoke();
+                if (results.Count > 0)
+                {
+
+                    form.Message = results[0].ToString();
+                }
+                else
+                {
+                    form.Message = "Nothing Returned";
+                }
+
+                form.Message = "Confirm The WiFi Module Is Plugged In and Test!";
+            }
+        }
+
+        internal void ChangeName(string text)
+        {
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+            {
+
+                runspace.Open();
+                //Pipeline pipeline = runspace.CreatePipeline(@"tzutil /s ""Central Standard Time""");
+                Pipeline pipeline = runspace.CreatePipeline(@"setcomputername " + text);
+
+                var results = pipeline.Invoke();
+                if (results.Count > 0)
+                {
+
+                    form.Message = results[0].ToString();
+                }
+                else
+                {
+                    form.Message = "Nothing Returned";
+                }
+
+            }
+        }
+
+
+        //public void SetPassword()
+        //{
+        //    using (Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo))
+        //    {
+
+        //        runspace.Open();
+        //        //string command = CommandBuilder.Reboot();
+        //        // string command = CommandBuilder.SetTime();
+        //        Pipeline pipeline = runspace.CreatePipeline("net user Administrator sparkpi");
+        //        //Pipeline pipeline = runspace.CreatePipeline("set-date \"Thursday, February 11, 2016 1:03:00 PM\"");
+        //        // Pipeline pipeline = runspace.CreatePipeline("Set-Item WSMan:\\localhost\\Client\\TrustedHosts -Value 10.0.99.97");
+
+        //        var results = pipeline.Invoke();
+
+        //        pipeline = runspace.CreatePipeline("get-date");
+        //        var respGetDate = pipeline.Invoke();
+        //        form.Message = respGetDate[0].ToString();
+        //        // p.CurrentDateTime = respGetDate[0].ToString();
+        //        //txtResult.Text = results.Count.ToString();
+        //        //intSuccess += 1;
+        //        // p.TimeUpdatedSuccessfully = true;
+        //        //p.TimeUpdatedSuccessfullyAt = DateTime.Now.ToLongTimeString();
+        //        //p.TimeUpdatedLastAtTicks = DateTime.Now.Ticks;
+
+        //    }
+        //}
     }
 }
